8d726c0fc2f09eaee33cc8010588fa8242046342
Elson Freire Filho <elsonfreirefilho@gmail.com>
2023-11-17 09:55:43 -0300

    added PlaylistModel, TrackModel

---
+++ src/br/ufrn/imd/model/PlaylistModel.java
@@ -0,0 +1,10 @@
+package br.ufrn.imd.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlaylistModel {
+
+	List<TrackModel> tracks = new ArrayList<TrackModel>();
+	
+}
---
+++ src/br/ufrn/imd/model/TrackModel.java
@@ -0,0 +1,8 @@
+package br.ufrn.imd.model;
+
+public class TrackModel {
+	
+	String name;
+	String artista;
+	
+}
