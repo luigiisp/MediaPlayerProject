598525317dc926038b3ce52c5abe02fedaf86a58
Luigi Soares <luigisp08@gmail.com>
2023-11-23 09:18:54 -0300

    Created PlayerModel and Stuctures of methods

---
+++ src/br/ufrn/imd/model/PlayerModel.java
@@ -0,0 +1,21 @@
+package br.ufrn.imd.model;
+
+public class PlayerModel {
+	QueueModel queue = new QueueModel();
+
+	public void playTrack() {
+	}
+
+	public void pauseTrack() {
+	}
+
+	public void nextTrack() {
+	}
+
+	public void backTrack() {
+	}
+
+	public void loopTrack() {
+	}
+
+}
