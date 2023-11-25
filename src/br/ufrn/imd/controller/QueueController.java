36c04bfe23dc3d9623dbb5b3fa679a4ddb22e5fa
Luigi Soares <luigisp08@gmail.com>
2023-11-25 15:28:50 -0300

    Updated QueueController

--- src/br/ufrn/imd/controller/QueueController.java
+++ src/br/ufrn/imd/controller/QueueController.java
@@ -1,5 +1,46 @@
 package br.ufrn.imd.controller;
 
-public class QueueController {
+import java.util.Collections;
 
+import br.ufrn.imd.model.PlaylistModel;
+import br.ufrn.imd.model.QueueModel;
+import br.ufrn.imd.model.TrackModel;
+
+public class QueueController {
+	QueueModel queue = new QueueModel();
+
+	public QueueController(QueueModel queue) {
+		super();
+		this.queue = queue;
+	}
+
+	public void addTrack(TrackModel track) {
+		queue.getTracks().add(track);
+	}
+
+	public void listTracks() {
+		for (TrackModel track : queue.getTracks()) {
+			System.out.println(track.getName());
+		}
+	}
+
+	public void removeTrack(TrackModel track) {
+		queue.getTracks().remove(track);
+	}
+
+	public void addPlaylist(PlaylistModel playlist) {
+		for (TrackModel track : playlist.getTracks()) {
+			queue.getTracks().add(track);
+		}
+	}
+
+	public void clearQueue() {
+		for (TrackModel track : queue.getTracks()) {
+			queue.getTracks().remove(track);
+		}
+	}
+
+	public void randomizeQueue() {
+		Collections.shuffle(queue.getTracks());
+	}
 }
--- src/br/ufrn/imd/model/QueueModel.java
+++ src/br/ufrn/imd/model/QueueModel.java
@@ -14,25 +14,4 @@ public class QueueModel {
 		return tracks;
 	}
 
-	// adds specific track to the queue
-	public void addTrack() {
-	}
-
-	// removes specific track from the queue
-	public void removeTrack() {
-	}
-
-	// adds all the tracks from the playlist to the queue
-	public void addPlaylist() {
-	}
-
-	// resets the queue by removing all tracks
-	public void reset() {
-		// for each track, removetrack
-	}
-
-	// randomly sorts the tracks in the queue
-	public void randomize() {
-
-	}
 }
