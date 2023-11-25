07fe912079f50a2522379b39b55c0e07974ecd21
Luigi Soares <luigisp08@gmail.com>
2023-11-25 16:08:25 -0300

    Updated Program.java

--- src/br/ufrn/imd/application/Program.java
+++ src/br/ufrn/imd/application/Program.java
@@ -1,9 +1,17 @@
 package br.ufrn.imd.application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Program {
 
 	public static void main(String[] args) {
-
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(""));
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
