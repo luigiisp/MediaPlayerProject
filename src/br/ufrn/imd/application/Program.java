a543b19aa441708a2a9e0f931231f9b43f09edb3
Luigi Soares <luigisp08@gmail.com>
2023-11-27 23:07:51 -0300

    Add directorio.txt

--- src/br/ufrn/imd/application/Program.java
+++ src/br/ufrn/imd/application/Program.java
@@ -1,13 +1,22 @@
 package br.ufrn.imd.application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import br.ufrn.imd.controller.TrackController;
-import javazoom.jl.player.Player;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = null;
 		String pathProject = System.getProperty("user.dir");
+		try {
+			reader = new BufferedReader(new FileReader(pathProject + "diretorio.txt"));
 
+			reader.close();
+		} catch (Exception e) {
+		}
 		TrackController trackController = new TrackController(pathProject + "//musicas");
 
 		System.out.println(trackController.getPath());
