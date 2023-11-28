301d493e8f8e96be4b31146f4b8eb8b4e20a9085
Elsoka <90570279+Elsoka@users.noreply.github.com>
2023-11-28 09:22:47 -0300

    Updated PlaylistModel and PlaylistController

--- src/br/ufrn/imd/model/PlaylistModel.java
+++ src/br/ufrn/imd/model/PlaylistModel.java
@@ -6,10 +6,23 @@ import java.util.UUID;
 
 public class PlaylistModel {
 
+	private String title;
 	private UUID uuid = UUID.randomUUID();
 	private List<TrackModel> tracks = new ArrayList<TrackModel>();
 
 	public PlaylistModel() {
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
 
 	public List<TrackModel> getTracks() {
